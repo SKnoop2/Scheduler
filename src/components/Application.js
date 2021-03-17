8a4524092dd9c6a0e08870f6d3ba149f815c28dc
Samantha Knoop <sknoop351@gmail.com>
2021-03-17 16:26:47 -0600

    adjust code to fetch days data from api server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
-import DayList from "components/DayList"
-import Appointment from "components/Appointment"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const appointments = [
   {
@@ -67,6 +50,16 @@ const appointments = [
 
 export default function Application() {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = "/api/days"
+    axios.get(url)
+      .then(res => {
+        setDays(res.data)
+      });
+  }, [])
+  
 
   const mappedAppts = appointments.map(appointments => {
     return (
@@ -75,8 +68,8 @@ export default function Application() {
         {...appointments}
       />
     )
-  })
-  
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
